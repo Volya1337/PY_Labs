1aec65fcc1179fa76e401e80dc0ea7deccaf0db7
Volya1337 <31896865+Volya1337@users.noreply.github.com>
2018-01-07 03:51:16 +0300

    Update main.py

--- lab-04/main.py
+++ lab-04/main.py
@@ -13,44 +13,42 @@ if num1 == 0:
     print("До свидания!")
 else:
     a = float(input("Введите а: "))
-    
+
     x_min = float(input("Введите минимальное значение x: "))
-    
+
     x_max = float(input("Введите макимальное значение х: "))
-    
+
     step_c = int(input("Введите число шагов: "))
     step = int
-    
-    step = (x_max - x_min) / (step_c - 1)
-    
+
     x = x_min
 
     if num1 == 1:
-        while x_min <= x_max:
-            d = 5*(a**2)+9*a*x_min-2*(x_min**2)
+        for i in range(step_c):
+            d = 5*(a**2)+9*a*x-2*(x**2)
             if isclose(d, 0, rel_tol=1e-09):
                 print(" Такого значения не существует... ")
             else:
-                G = ((-2 * (-5 * (a ** 2) + 3 * a * x_min + 2 * (x_min ** 2))) / d)
+                G = ((-2 * (-5 * (a ** 2) + 3 * a * x + 2 * (x ** 2))) / d)
                 res.append(G)
                 print("x =", x, "\tG =", G)
-                x_min += step
+                x += (x_max - x_min) / (step_c - 1)
     elif num1 == 2:
-        while x_min <= x_max:
-            d = pi*(10*(a**2)+37*a*x_min+7*(x_min**2))
+        for i in range(step_c):
+            d = pi*(10*(a**2)+37*a*x+7*(x**2))
             if isclose(d, 0, rel_tol=1e-09):
                 print(" Такого значения не существует... ")
             else:
-                F = (sin(pi*(10*(a**2)+37*a*x_min+7*(x_min**2))) / d)
+                F = (sin(pi * (10 * (a**2) + 37 * a * x + 7 * (x**2))) / d)
                 res.append(F)
                 print("x =", x, "\tF =", F)
-                x_min += step
+                x += (x_max - x_min) / (step_c - 1)
     elif num1 == 3:
-        while x_min <= x_max:
-                Y = (log(-5*(a**2)-16*a*x_min+16*(x_min**2)+1) / log(2))
+        for i in range(step_c):
+                Y = (log(-5 * (a**2) - 16 * a * x + 16 * (x**2) + 1) / log(2))
                 res.append(Y)
                 print("x =", x, "\tY =", Y)
-                x_min += step
+                x += (x_max - x_min) / (step_c - 1)
 
-    print("Max.res. = ", max(res))
-    print("Min.res. = ", min(res))
+print("Max.res. = ", max(res))
+print("Min.res. = ", min(res))
